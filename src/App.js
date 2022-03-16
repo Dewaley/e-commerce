a6475ecbad86430b8b7701989371c7dc927e0fb4
Dewaley <adewaleademilua@gmail.com>
2022-03-16 23:57:10 +0100

    Nav bar template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-slate-900 text-6xl'>Hello World</h1>
+      
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Shopping</h1>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <ul>
+        <li>login</li>
+        <li>cart</li>
+      </ul>
+      ;{' '}
+    </div>
+  );
+};
+
+export default Navbar;
