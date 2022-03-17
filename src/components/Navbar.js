b6b44d035c8fc914525ffde414b107783fb83549
Dewaley <adewaleademilua@gmail.com>
2022-03-17 09:07:11 +0100

    Navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Shopping</h1>
-      <ul>
+    <div className='flex justify-between py-3 px-5 border-b-teal-400 border-b-2 items-center'>
+      <h1 className='text-3xl'>Shopping</h1>
+      <ul className='flex gap-5'>
         <li>Home</li>
         <li>Products</li>
         <li>About</li>
         <li>Contact</li>
       </ul>
-      <ul>
+      <ul className='flex gap-5'>
         <li>login</li>
         <li>cart</li>
       </ul>
-      ;{' '}
     </div>
   );
 };
