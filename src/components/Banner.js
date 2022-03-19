a260334a940f55f35a3e8e44dbac663c392bdb01
Dewaley <adewaleademilua@gmail.com>
2022-03-19 02:29:02 +0100

    Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='h-60 bg-hero-background bg-cover bg-no-repeat flex flex-col justify-center px-4  text-white '>
+    <div className='h-60 sm:h-80 md:h-96 bg-hero-background bg-cover bg-no-repeat flex flex-col justify-center px-4  text-white '>
      <p className='uppercase text-2xl'>New Arrivals</p>
      <p className='text-lg'>Check out all of the trends</p>
     </div>
