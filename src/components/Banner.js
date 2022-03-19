68271823d79f0718c613fdf63a85f1831e5430db
Dewaley <adewaleademilua@gmail.com>
2022-03-19 07:55:24 +0100

    Banner styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='h-60 sm:h-80 md:h-96 bg-hero-background bg-cover bg-no-repeat flex flex-col justify-center px-4  text-white '>
-     <p className='uppercase text-2xl'>New Arrivals</p>
-     <p className='text-lg'>Check out all of the trends</p>
+     <p className='uppercase text-4xl sm:text-6xl'>New Arrivals</p>
+     <p className='text-xl sm:text-3xl'>Check out all of the trends</p>
     </div>
   )
 }
